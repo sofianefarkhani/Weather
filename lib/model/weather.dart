4a3eeac82386e998ee7a745e38b28449a3d5a7e9
Sofiane Farkhani <farkhans@3il.fr>
2022-01-11 22:30:46 +0100

    feat: ajout de Google Map

--- lib/model/weather.dart
+++ lib/model/weather.dart
@@ -1,1 +1,13 @@
-class Weather {}
+class Weather {
+  Weather({this.name});
+
+  String? name;
+
+  static Weather fromJSON(Map<String, dynamic> json) =>
+      Weather(name: json['name']);
+
+  static Weather fromFirestore(Object data) {
+    final Map<String, dynamic> dataMap = data as Map<String, dynamic>;
+    return Weather.fromJSON(dataMap);
+  }
+}
--- lib/ui/weatherforecast/weatherforecast_view.dart
+++ lib/ui/weatherforecast/weatherforecast_view.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:weather/di/dependency_graph.dart';
+import 'package:weather/services/authentication_service.dart';
 
 class WeatherForecastPage extends StatelessWidget {
   const WeatherForecastPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // ignore: prefer_const_constructors
     return Scaffold(
-      body: const Center(
-        child: Text("Page avec la liste des météos"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Je suis connecté"),
+            Text(locator<AuthenticationService>().weatherUser != null
+                ? locator<AuthenticationService>().weatherUser!.name ?? "N/A"
+                : "N/A"),
+            const Text("Page avec la liste des météos"),
+            ElevatedButton(
+              child: const Text("Se déconnecter"),
+              onPressed: () {
+                locator<AuthenticationService>().logout();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
