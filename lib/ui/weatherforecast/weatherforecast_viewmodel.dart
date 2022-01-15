e86c1703987bcecb8d551323cdfe5ae828d2f905
Sofiane Farkhani <farkhans@3il.fr>
2022-01-15 10:37:55 +0100

    feat: ajout du carousel

--- lib/ui/weatherforecast/weatherforecast_viewmodel.dart
+++ lib/ui/weatherforecast/weatherforecast_viewmodel.dart
@@ -1,7 +1,8 @@
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_options.dart';
 import 'package:injectable/injectable.dart';
 import 'package:stacked/stacked.dart';
 import 'package:weather/services/apiWeather_service.dart';
-
 
 @injectable
 class WeatherForeastViewModel extends BaseViewModel {
@@ -9,8 +10,15 @@ class WeatherForeastViewModel extends BaseViewModel {
 
   WeatherForeastViewModel(this._meteo);
 
+  int _currentIndex = 0;
+  int get currentIndex => _currentIndex;
+
+  final CarouselController _carouselController = CarouselController();
+  CarouselController get carouselController => _carouselController;
+
   Future initialize() async {
     await _meteo.getMeteoInTime("Limoges");
   }
 
+  carouselChangeCity(int index, CarouselPageChangedReason reason) {}
 }
